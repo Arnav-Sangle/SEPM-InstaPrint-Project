6494cd276c092791e1df506066b1292a732a67ca
Arnav-Sangle <arnavsangle7x24@gmail.com>
2024-04-11 18:43:56 +0530

    added _isPressed effect to my_button.dart & default_button.dart

--- lib/components/default_button.dart
+++ lib/components/default_button.dart
@@ -1,27 +1,55 @@
 import 'package:flutter/material.dart';
 
-class DefaultButton extends StatelessWidget {
+class DefaultButton extends StatefulWidget {
   final Function()? onTap;
   final String text;
 
   const DefaultButton({Key? key, this.onTap, this.text = ''}) : super(key: key);
 
   @override
+  _DefaultButtonState createState() => _DefaultButtonState();
+}
+
+class _DefaultButtonState extends State<DefaultButton> {
+  bool _isPressed = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTapDown: (_) {
+        setState(() {
+          _isPressed = true;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          _isPressed = false;
+        });
+        if (widget.onTap != null) {
+          widget.onTap!();
+        }
+      },
+      onTapCancel: () {
+        setState(() {
+          _isPressed = false;
+        });
+      },
       child: Container(
-        width: MediaQuery.of(context).size.width/1.7,
-        height: MediaQuery.of(context).size.height/14,
+        width: MediaQuery.of(context).size.width / 1.7,
+        height: MediaQuery.of(context).size.height / 14,
         padding: const EdgeInsets.all(20),
         margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: _isPressed ? Colors.grey[200] : Colors.white,
           borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: _isPressed ? Colors.grey[300]! : Colors.transparent,
+            width: 1,
+          ),
         ),
         child: Center(
           child: Text(
-            text,
+            widget.text,
             style: TextStyle(
               color: Colors.purple,
               fontSize: 14,
--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,31 +1,57 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget {
+class MyButton extends StatefulWidget {
   final Function()? onTap;
   final String text;
 
   const MyButton({Key? key, this.onTap, this.text = ""}) : super(key: key);
 
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  bool _isPressed = false;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTapDown: (_) {
+        setState(() {
+          _isPressed = true;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          _isPressed = false;
+        });
+        if (widget.onTap != null) {
+          widget.onTap!();
+        }
+      },
+      onTapCancel: () {
+        setState(() {
+          _isPressed = false;
+        });
+      },
       child: Container(
         width: MediaQuery.of(context).size.width,
         padding: const EdgeInsets.all(25),
         margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(8)
+          color: _isPressed ? Colors.grey[900] : Colors.black, // Change color when pressed
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: _isPressed
+              ? [BoxShadow(color: Colors.black.withOpacity(0.5), blurRadius: 4, offset: Offset(0, 2))]
+              : [], // Add shadow when pressed
         ),
         child: Center(
           child: Text(
-            text,
+            widget.text,
             style: TextStyle(
               color: Colors.white,
               fontSize: 16,
-              fontWeight: FontWeight.bold
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
