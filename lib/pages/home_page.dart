b613ffcd07b5f88b0814ea9b137a9ac847f2e7da
Arnav-Sangle <arnavsangle7x24@gmail.com>
2024-04-11 16:18:23 +0530

    created rough home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,8 +5,39 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      child: Text("Hello, how are you?")
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to InstaPrint!'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Custom Image
+            Image.asset(
+              'assets/images/instaPrintLogo.png', // Replace this with the path to your custom image
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 50),
+            // Section 1: Create print request
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to create print request page
+              },
+              child: Text('Create Print Request'),
+            ),
+            SizedBox(height: 20),
+            // Section 2: Review previous requests
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to review previous requests page
+              },
+              child: Text('Review Previous Requests'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
